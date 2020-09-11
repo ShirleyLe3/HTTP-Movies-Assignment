bbfc8f079e3babb8aec127a0197f62ecfe010012
Shirley Lee <synchrotronflux@yahoo.com>
2020-09-10 21:27:41 -0400

    MVP+bug

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,24 @@ import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
-import axios from 'axios';
+import MovieUpdate from "./Movies/MovieUpdate";
+import axios from "axios";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
   const [movieList, setMovieList] = useState([]);
-
-  const getMovieList = () => {
-    axios
-      .get("http://localhost:5000/api/movies")
-      .then(res => setMovieList(res.data))
-      .catch(err => console.log(err.response));
-  };
-
-  const addToSavedList = movie => {
-    setSavedList([...savedList, movie]);
-  };
+  const [items, setItems] = useState([]);
 
   useEffect(() => {
-    getMovieList();
+    axios
+      .get("http://localhost:5000/api/movies")
+      .then((res) => setItems(res.data))
+      .catch((error) => console.log(error));
   }, []);
 
+  const addToSavedList = (movie) => {
+    setSavedList([...savedList, movie]);
+  };
   return (
     <>
       <SavedList list={savedList} />
